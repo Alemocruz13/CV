74ffcf84b210a95877aecfecd79d7ecf2442b1e0
Alemocruz13 <108594843+Alemocruz13@users.noreply.github.com>
2024-09-24 20:15:24 -0600

    Add files via upload

---
+++ CV.js
@@ -0,0 +1,5 @@
+// Puedes agregar interactividad aquí si lo deseas
+// Por ejemplo, puedes mostrar una alerta cuando el usuario haga clic en un botón
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Currículum cargado!");
+});
